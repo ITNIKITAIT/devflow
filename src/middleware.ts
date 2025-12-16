88ec9cb6c1f46dbd41708feacd0baa4dade4e1f7
ITNIKITAIT <nikito4ka281205@gmail.com>
2025-12-16 16:53:13 +0200

    feat: add middleware for login page

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse, type NextRequest } from 'next/server';
+
+import { auth } from '@/lib/auth';
+
+export default async function middleware(request: NextRequest) {
+  const session = await auth();
+  if (session && session.user) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/login'],
+  runtime: 'nodejs',
+};
